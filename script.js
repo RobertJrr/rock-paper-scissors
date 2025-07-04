55ded42cb9b4324297e7740b5c5453ad626e5c7b
Robert M <99871728+Robert010600@users.noreply.github.com>
2025-07-03 23:49:19 -0700

    Fixed logic and added DOM methods

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//Rock, Paper, Scissors Game
+//Player and Computer
+
+//Create logic to get winner of round
+//Scissor beats paper, paper beats rock and rock beats scissors
+
+//Keep track of the points per player for each win
+
+//Display the winner of total game and reset the game
+
+
+
 //function to get computerChoice
 function getComputerChoice(){
     // Get numbers between 1 2 and 3
@@ -5,7 +17,7 @@ function getComputerChoice(){
 }
 
 //function to get human choice
-function getHumanChoice(){
+function getplayerChoice(){
     let choice = prompt("Enter choice: ");
     return choice;
 
@@ -16,124 +28,125 @@ function getChoice(){
     // Get numbers between 1 2 and 3
     let choice = Math.floor(Math.random() * 3 + 1);
     if(choice === 1){
-        return "Rock";
+        return "rock";
     }else if(choice === 2){
-        return "Paper";
+        return "paper";
     }else{
-        return "Scissor";
+        return "scissors";
     }
 }
 
 
-function updateWinnerScore(score){
-    return ++score;
-}
-
-function displayPoints(humanScore,computerScore){
-    const p = document.querySelectorAll("p#player, p#computer");
-    //updating winner score to display
-    p.forEach((score)=>{
-        if(score.id === "player"){
-            score.textContent = "Player Score: " + humanScore;
-        }else{
-            score.textContent = "Computer Score: " + computerScore;
-        }
-    })
-
-}
-
 //function to play round
-function playRound(humanChoice, computerChoice){
+function playRound(playerChoice,computerChoice){
 
+    //set to lowerCase
+    playerChoice = playerChoice.toLowerCase();
 
-    //ensuring choices are set to lowercase
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-
-    //creating element to display round result 
-    const resultDisplay = document.createElement("p");
-
-
-    //outcome string
+    //text for roundWinner to return from function 
     let roundWinner = "";
-    
+    //text to display winner of round
+    let roundText = "";
+
+
     switch(true){
         //cases that computer wins
-        case (humanChoice === "rock" && computerChoice === "paper"):
-        case (humanChoice === "scissor" && computerChoice === "rock"):
-        case (humanChoice === "paper" && computerChoice === "scissor"):
+        case (playerChoice === "rock" && computerChoice === "paper"):
+        case (playerChoice === "scissors" && computerChoice === "rock"):
+        case (playerChoice === "paper" && computerChoice === "scissors"):
             //displaying result of round
-            console.log("You lost! " + computerChoice + " beats " + humanChoice);
-            //setting a string for the winner
             roundWinner = "computer";
+            roundText = "LOSER! " + "Player: " + playerChoice + " | Computer: " + computerChoice;
+            displayRoundWinner(roundText);
             break;
-        //cases that human wins
-        case (humanChoice === "paper" && computerChoice === "rock"):
-        case (humanChoice === "rock" && computerChoice === "scissor"):
-        case (humanChoice === "scissor" && computerChoice === "paper"):
-
-            //displaying and updating score
-            console.log("You won! " + humanChoice + " beats " + computerChoice);
-            roundWinner = "human";
+        //cases that player wins
+        case (playerChoice === "paper" && computerChoice === "rock"):
+        case (playerChoice === "rock" && computerChoice === "scissors"):
+        case (playerChoice === "scissors" && computerChoice === "paper"):
+            ////displaying result of round
+            roundWinner = "player"
+            roundText = "WINNER! " + "Player: " + playerChoice + " | Computer: " + computerChoice;
+            displayRoundWinner(roundText);
             break;
         default:
-            console.log("Tie: " + humanChoice + " | " + computerChoice);
-            roundWinner = "tie";
+            //displaying result of round
+            roundText = "TIE! " + "Player: " + playerChoice + " | Computer: " + computerChoice;
+            displayRoundWinner(roundText);
     }
     return roundWinner;
 }
 
 function playGame(){
 
-    let humanScore = 0;
+    const totalrounds = 5;
+    let playerScore = 0;
     let computerScore = 0;
 
-    ///getting list of buttons
-    const buttons = document.querySelectorAll("button");
+    //Get reference to button nodes to play each round
+    const playButtons = document.querySelectorAll("button");
 
-    //calling the play game function and passing chosen button from player
-    buttons.forEach((button)=>{
-        button.addEventListener("click",(event)=>{
+    //Get reference to the result text
+    const playerResultElement = document.querySelector("#player");
+    const computerResultElement = document.querySelector("#computer");
 
-            //getting choices
-            const humanChoice = button.textContent;
-            const computerChoice = getComputerChoice();
-            let outcome = playRound(humanChoice,computerChoice);
+    //maintain original text of results
+    const originPlayerText = playerResultElement.textContent;
+    const originComputerText = computerResultElement.textContent;
 
-            //updating the winner score
-            if(outcome === "human"){
-                humanScore = updateWinnerScore(humanScore);
-            }else if(outcome === "computer"){
-                computerScore = updateWinnerScore(computerScore);
+    //element to be created and append when there is a winner
+    const winnerText = document.createElement("p");
+
+    //event listener for buttons being clicked
+    playButtons.forEach((button)=>{
+        button.addEventListener("click",()=>{
+            //Getting choices for player and human
+            let playerChoice = button.textContent;
+            let computerChoice = getComputerChoice();
+            //playing round
+            let roundWinner = playRound(playerChoice,computerChoice);
+            
+            //update score of round winner winner
+            if(roundWinner === "player"){
+                playerScore++;
+                updateResults(playerResultElement, originPlayerText,playerScore);
+            }else if(roundWinner === "computer"){
+                computerScore++;
+                updateResults(computerResultElement,originComputerText,computerScore);
             }
 
-            //update points here
-            displayPoints(humanScore,computerScore);
+            //if one of players hits 5 points, display the GAME WINNER
+            //maybe add this outside this event handler and listener
+            
+            //reset the score
+            if(playerScore > totalrounds || computerScore > totalrounds){
+                updateResults(playerResultElement,originPlayerText,0);
+                updateResults(computerResultElement,originComputerText,0);
+            }
 
-            endGame(humanScore,computerScore);
-            return;
         });
     });
+
+}
+
+function displayRoundWinner(roundText){
+    //create element and display
+    const pText = document.createElement("p");
+    //get reference to a div
+    const divResult = document.querySelector("#results");
+    pText.textContent = roundText
+    divResult.appendChild(pText);
+
+    //remove element after 5000 milliseconds
+    setTimeout(function(){
+        pText.remove();
+    },3000);
 }
 
 
-function endGame(humanScore, computerScore){
+function updateResults(resultElement, originText,score){
+    resultElement.textContent = originText + score;
 
-    const displayWinner = document.createElement("p");
-
-    if(humanScore == 5){
-        displayWinner.textContent = "YOU WON!!!!!";
-    }else if(computerScore == 5){
-        displayWinner.textContent = "YOU LOST!!!!";
-    }
-
-    //appending result to end of result div
-    const resultDiv = document.getElementById("results");
-    resultDiv.appendChild(displayWinner);
 }
-
-
-
 
 function main(){
     playGame();
