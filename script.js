d9bad886236faa99bedf4e673f817d8f60b46857
Robert M <99871728+Robert010600@users.noreply.github.com>
2025-07-04 20:16:26 -0700

    Reworked the logic and correctly updating score now

--- script.js
+++ script.js
@@ -16,13 +16,6 @@ function getComputerChoice(){
     return getChoice();
 }
 
-//function to get human choice
-function getplayerChoice(){
-    let choice = prompt("Enter choice: ");
-    return choice;
-
-}
-
 //function to get choice
 function getChoice(){
     // Get numbers between 1 2 and 3
@@ -36,65 +29,91 @@ function getChoice(){
     }
 }
 
+//function to display who wins the round
+function disgetWinnerWinner(roundWinner,playerChoice,computerChoice,roundArray){
+    const roundText = document.createElement("h1");
+    const divRoundResult = document.querySelector("#roundresult");
+
+    if(roundWinner === "tie"){
+        roundText.textContent = "ITS A TIE!";
+        divRoundResult.appendChild(roundText);
+        setTimeout(function(){
+            roundText.remove();
+        },2000);
+        return;
+    }
+   
+    roundText.textContent = "Player: " + playerChoice + " | Computer: " + computerChoice;
+    divRoundResult.appendChild(roundText);
+
+     setTimeout(function(){
+            roundText.remove();
+    },2000);
+
+}
+
 
 //function to play round
-function playRound(playerChoice,computerChoice){
+function getWinner(playerChoice,computerChoice){
 
     //set to lowerCase
     playerChoice = playerChoice.toLowerCase();
 
-    //text for roundWinner to return from function 
-    let roundWinner = "";
-    //text to display winner of round
-    let roundText = "";
+    //to return string of winner
+    if((playerChoice === "rock" && computerChoice === "scissors")||
+         (playerChoice === "scissors" && computerChoice === "paper") ||
+         (playerChoice === "rock" && computerChoice === "paper")){
+            return "player";
+        }else if(playerChoice === computerChoice){
+            return "tie";
+        }else{
+            return "computer";
+        }
+}
+
+function final(winner,gameWinnertext){
+    //create winning text
+    gameWinnertext.textContent = winner + " WINS! Press reset to play again!";
+    const finalElem = document.querySelector("#final");
+    finalElem.appendChild(gameWinnertext);
+
+    setTimeout(function(){
+        gameWinnertext.remove();
+    },4000);
 
 
-    switch(true){
-        //cases that computer wins
-        case (playerChoice === "rock" && computerChoice === "paper"):
-        case (playerChoice === "scissors" && computerChoice === "rock"):
-        case (playerChoice === "paper" && computerChoice === "scissors"):
-            //displaying result of round
-            roundWinner = "computer";
-            roundText = "LOSER! " + "Player: " + playerChoice + " | Computer: " + computerChoice;
-            displayRoundWinner(roundText);
-            break;
-        //cases that player wins
-        case (playerChoice === "paper" && computerChoice === "rock"):
-        case (playerChoice === "rock" && computerChoice === "scissors"):
-        case (playerChoice === "scissors" && computerChoice === "paper"):
-            ////displaying result of round
-            roundWinner = "player"
-            roundText = "WINNER! " + "Player: " + playerChoice + " | Computer: " + computerChoice;
-            displayRoundWinner(roundText);
-            break;
-        default:
-            //displaying result of round
-            roundText = "TIE! " + "Player: " + playerChoice + " | Computer: " + computerChoice;
-            displayRoundWinner(roundText);
-    }
-    return roundWinner;
 }
 
 function playGame(){
 
-    const totalrounds = 5;
+    //number of rounds to win
+    const roundsToWin = 5;
+
+    //current score for computer and player
     let playerScore = 0;
     let computerScore = 0;
 
+    //get reference to text
+    const playerText = document.querySelector("#player");
+    const computerText = document.querySelector("#computer");
+
+    //maintaining initial score text
+    let initialPlayerText = playerText.textContent;
+    let initialComputerText = computerText.textContent;
+
+    //setting initial score to the player and computer text
+    playerText.textContent = initialPlayerText + playerScore;
+    computerText.textContent = initialComputerText + computerScore;
+
     //Get reference to button nodes to play each round
-    const playButtons = document.querySelectorAll("button");
+    const playButtons = document.querySelectorAll("button#rock, button#paper, button#scissors");
 
-    //Get reference to the result text
-    const playerResultElement = document.querySelector("#player");
-    const computerResultElement = document.querySelector("#computer");
+    //reference to reset button
+    const resetButton = document.querySelector("button#reset");
 
-    //maintain original text of results
-    const originPlayerText = playerResultElement.textContent;
-    const originComputerText = computerResultElement.textContent;
+    //display winner
+    const gameWinnertext = document.createElement("h2");
 
-    //element to be created and append when there is a winner
-    const winnerText = document.createElement("p");
 
     //event listener for buttons being clicked
     playButtons.forEach((button)=>{
@@ -102,49 +121,39 @@ function playGame(){
             //Getting choices for player and human
             let playerChoice = button.textContent;
             let computerChoice = getComputerChoice();
-            //playing round
-            let roundWinner = playRound(playerChoice,computerChoice);
-            
-            //update score of round winner winner
-            if(roundWinner === "player"){
+            //playing round based on choices
+            let roundWinner = getWinner(playerChoice,computerChoice);
+
+            //update score for round winner
+            if(roundWinner === "player" && playerScore < roundsToWin){
                 playerScore++;
-                updateResults(playerResultElement, originPlayerText,playerScore);
-            }else if(roundWinner === "computer"){
+                playerText.textContent = initialPlayerText + playerScore;
+                disgetWinnerWinner(roundWinner,playerChoice,computerChoice);
+            }else if(roundWinner === "computer" & computerScore < roundsToWin){
                 computerScore++;
-                updateResults(computerResultElement,originComputerText,computerScore);
+                computerText.textContent = initialComputerText + computerScore;
+                disgetWinnerWinner(roundWinner,playerChoice,computerChoice);
+            }else{
+                disgetWinnerWinner(roundWinner,playerChoice,computerChoice);
             }
-
-            //if one of players hits 5 points, display the GAME WINNER
-            //maybe add this outside this event handler and listener
-            
-            //reset the score
-            if(playerScore > totalrounds || computerScore > totalrounds){
-                updateResults(playerResultElement,originPlayerText,0);
-                updateResults(computerResultElement,originComputerText,0);
+            //display game winner
+            if(playerScore == roundsToWin){
+                final(roundWinner,gameWinnertext);
+            }else if(computerScore == roundsToWin){
+                final(roundWinner,gameWinnertext);
             }
-
         });
     });
+    
+    //resetting the game upon click of reset button
+    resetButton.addEventListener("click",()=>{
+        playerScore = 0;
+        computerScore = 0;
 
-}
+        playerText.textContent = initialPlayerText + playerScore;
+        computerText.textContent = initialComputerText + computerScore;
+    });
 
-function displayRoundWinner(roundText){
-    //create element and display
-    const pText = document.createElement("p");
-    //get reference to a div
-    const divResult = document.querySelector("#results");
-    pText.textContent = roundText
-    divResult.appendChild(pText);
-
-    //remove element after 5000 milliseconds
-    setTimeout(function(){
-        pText.remove();
-    },3000);
-}
-
-
-function updateResults(resultElement, originText,score){
-    resultElement.textContent = originText + score;
 
 }
 
