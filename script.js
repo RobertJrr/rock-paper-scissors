8c00f46bdd16be0634d9e204fb873643d029bcb8
Robert M <99871728+Robert010600@users.noreply.github.com>
2025-07-14 03:20:04 -0700

    Added functionality to completely stop game once score limit reached

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Rock, Paper, Scissors Game
-//Player and Computer
 
+
+//psuedocode
+//Player and Computer
 //Create logic to get winner of round
 //Scissor beats paper, paper beats rock and rock beats scissors
-
 //Keep track of the points per player for each win
-
 //Display the winner of total game and reset the game
 
 
@@ -56,38 +56,52 @@ function displaySelections(playerChoice,computerChoice){
     roundResultList.appendChild(newLi);
 
 }
-
-//left here, add some functionality to display the choices selected per round
+//function to play a round
 function playRound(playerChoice, computerChoice){
-    playerChoice = getChoice();
+    //get choices
     computerChoice = getComputerChoice();
+
     //display selection for round
     displaySelections(playerChoice,computerChoice);
 
     //calling function getWinner to get winner of round
     return getWinner(playerChoice,computerChoice);
-
 }
 
+//display winner and update winner score
+function displayAndUpdateRoundWinner(roundWinner,score,scoreText,scoreTextReference){
+    const roundWinnerElem = document.querySelector("#roundwinner");
+    //if score is less than 4 display round winner
+    if(score < 4){
+        let roundWinnerString = roundWinner + " wins this round!";
+        roundWinnerElem.textContent = roundWinnerString.toUpperCase();
+    }else if(score == 5){ //display GAME WINNER at 5 points
+        roundWinnerElem.textContent = roundWinner.toUpperCase() + " WINS!";
+    }
 
-//update score for winner 
-function updateAndDisplayRoundWinner(roundWinner){
+    
+    //update score
+    updateScoreText(score,scoreText,scoreTextReference);
+}
+
+//put this in updateWinnerFunction
+function updateScoreText(score,scoreText,scoreTextReference){
+    scoreTextReference.textContent = scoreText + score;
 
 }
 
 
 function playGame(){
 
+    //total rounds
+    const totalRounds = 5;
 
     //computer choice and human choice
     let playerChoice = "";
     let computerChoice = "";
 
     //variable for round winner
-    let roundWinner = "default";
-
-    //number of rounds to win
-    const roundsToWin = 5;
+    let roundWinner = "";
 
     //current score for computer and player
     let playerScore = 0;
@@ -97,53 +111,68 @@ function playGame(){
     const playerText = document.querySelector("#player");
     const computerText = document.querySelector("#computer");
 
+    //original player and computer string to hold
+    let playerScoreString = playerText.textContent;
+    let computerScoreString = computerText.textContent;
+
+    //initial score display
+    playerText.textContent = playerScoreString + playerScore;
+    computerText.textContent = computerScoreString + computerScore;
+
+
     //Get reference to button nodes to play each round
     const playButtons = document.querySelectorAll("button#rock, button#paper, button#scissors");
     //reference to reset button
     const resetButton = document.querySelector("button#reset");
 
-    //maintaining initial score text
-    let initialPlayerText = playerText.textContent;
-    let initialComputerText = computerText.textContent;
-    //setting initial score to the player and computer text
-    playerText.textContent = initialPlayerText + playerScore;
-    computerText.textContent = initialComputerText + computerScore;
-
-
-
+    
 
     //add event listener for each button
     playButtons.forEach((button)=>{
         button.addEventListener("click",function(){
+            //to stop the game until reset is hit!
+            if(playerScore >= totalRounds || computerScore >= totalRounds){
+                return;
+            }
             if(button.id === "rock"){
+                playerChoice = button.id;
                 roundWinner = playRound(playerChoice,computerChoice);
             }else if(button.id === "paper"){
+                playerChoice = button.id;
                 roundWinner = playRound(playerChoice,computerChoice);
             }else if(button.id === "scissors"){
+                playerChoice = button.id;
                roundWinner = playRound(playerChoice,computerChoice);
             }
             //update winner score 
             if(roundWinner === "player"){
                 playerScore++;
-                //update score text (DO THIS NEXT)
+                //show round winner and update score text
+                displayAndUpdateRoundWinner(roundWinner,playerScore,playerScoreString,playerText);
             }else if(roundWinner === "computer"){
                 computerScore++;
-                //update score text 
+                //update score text
+                displayAndUpdateRoundWinner(roundWinner,computerScore,computerScoreString,computerText);
             }
 
         });
 
     });
 
-
-
-
     //resetting the game upon click of reset button
     resetButton.addEventListener("click",()=>{
         playerScore = 0;
         computerScore = 0;
-        playerText.textContent = initialPlayerText + playerScore;
-        computerText.textContent = initialComputerText + computerScore;
+        playerText.textContent = playerScoreString + playerScore;
+        computerText.textContent = computerScoreString + computerScore;
+
+        //ref to ul element and remove all child elements
+        const ulElement = document.querySelector("#resultlist");
+        ulElement.innerHTML = "";
+
+        //ref to roundwinnerwrapper element and remove child element
+        const roundElement = document.querySelector("#roundwinner");
+        roundElement.innerHTML = "";
     });
 
 
